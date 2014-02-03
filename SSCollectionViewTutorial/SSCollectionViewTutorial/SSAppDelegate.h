94770b3d1cc81d86efd865a7c1ff8bc8f8cb950f
apple 02 <apple02@BSAs-iMac-52.local>
2014-02-03 15:35:12 +0530

    committed

---
+++ SSCollectionViewTutorial/SSCollectionViewTutorial/SSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SSAppDelegate.h
+//  SSCollectionViewTutorial
+//
+//  Created by apple 02 on 30/01/14.
+//  Copyright (c) 2014 BSA Univ2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SSCollectionViewTutorial/SSCollectionViewTutorial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SSCollectionViewTutorial
+//
+//  Created by apple 02 on 30/01/14.
+//  Copyright (c) 2014 BSA Univ2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SSAppDelegate class]));
+    }
+}
